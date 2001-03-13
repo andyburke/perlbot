2d08d23a67fe938d80f572353bc63e44b8c66c4c
Jeremy Muhlich <jmuhlich@bitflood.org>
2001-03-13 03:30:59 +0000

    added Cal plugin

---
+++ perlbot/plugins/Cal/Plugin.pm
@@ -0,0 +1,71 @@
+# Cal
+# Jeremy Muhlich
+#
+# calls the unix util 'cal' to print out a calendar
+
+package Cal::Plugin;
+
+use Perlbot;
+use POSIX;
+
+sub get_hooks {
+  return { public => \&on_public, msg => \&on_msg};
+}
+
+sub on_public {
+  my $conn = shift;
+  my $event = shift;
+
+  if ($event->{args}[0] =~ /^!cal\s*/) {
+    cal($conn, $event, $event->{to}[0]);
+  }
+}  
+
+sub on_msg {
+  my $conn = shift;
+  my $event = shift;
+
+  if ($event->{args}[0] =~ /^!cal\s*/) {
+    cal($conn, $event, $event->nick);
+  }
+}  
+
+sub cal {
+  my $conn = shift;
+  my $event = shift;
+  my $who = shift;
+  my $in;
+
+  if (!defined($pid = fork)) {
+    $conn->privmsg($chan, "error in cal...");
+    return;
+  }
+
+  if ($pid) {
+    # parent
+
+    $SIG{CHLD} = sub { wait; };
+    return;
+
+  } else {
+    # child...
+
+    ($in = $event->{args}[0]) =~ s/^!cal\s*//;
+
+    $in =~ s/\`//g; #security?
+    $in =~ s/\$//g;
+    $in =~ s/\|//g;
+    
+    my @cal = `cal $in`;
+    chomp @cal;
+
+    foreach(@cal) {
+      $conn->privmsg($who, $_);
+    }
+    
+    $conn->{_connected} = 0;
+    exit 0;			# kill child
+  }
+}
+
+1;
