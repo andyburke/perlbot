be7ab39070a8b741516616f1f49ac998b2536676
Andy Burke <aburke@bitflood.org>
2002-01-22 22:09:37 +0000

    little changes

--- perlbot/plugins/Protection/Plugin.pm
+++ perlbot/plugins/Protection/Plugin.pm
@@ -14,49 +14,41 @@ sub modechange {
     my $mintime = 10;
     my $maxdeops = 2;
     my $timedif = 0;
-
+    
     my $deop_ratio = $maxdeops / $mintime;
-
+    
     my $curnick = (split('!', $event->{from}))[0];
-
+    
     my $curchan = $event->{to}[0];
-
+    
     my $curmode = $event->{args}[0];
     my $curtarget = $event->{args}[1];
+    
+    my $deops = length(($curmode =~ /-(o+)/g)[0]);
+
+    if(!$deops) { # they didn't deop anyone, we can ignore
+      return;
+    }
 
     $current_mode_change_time = time();
-
-    if(!$last_mode_change_time || !%last_mode_changer) {
-
-	$last_mode_change_time = $current_mode_change_time;
     
+    if(!$last_mode_change_time || !%last_mode_changer) {
+	$last_mode_change_time = $current_mode_change_time;
+	
 	$last_mode_changer{'nick'} = $curnick;
 	$last_mode_changer{'chan'} = $curchan;
 	$last_mode_changer{'mode'} = $curmode;
 	$last_mode_changer{'target'} = $curtarget;
-
-	return;
+        $last_mode_changer{'deops'} = $deops;
     }
-
+    
     $timedif = $current_mode_change_time - $last_mode_change_time;
-
+    if($timedif < 1) { $timedif = 1; }
+    
     if($last_mode_changer{'nick'} eq $curnick) {
-#	if(($curmode eq '-o') && ($last_mode_changer[$mode] eq '-o')) {
-#	    if($timedif < $mintime) {
-#		$conn->mode($curchan, "-o", $curnick);
-#		return;
-#	    }
-#	}
-
-      if(($curmode =~ /-o/) && ($last_mode_changer{'mode'} =~ /-o/)) {
-	$curmode =~ s/[+|-]//g;
-	my @modes = split(/\s*/, $curmode);
-
-	if((@modes / $mintime) > $deop_ratio) {
-	  $conn->mode($curchan, "-o", $curnick);
+	if(($deops / $timedif) > $deop_ratio) {
+	    $conn->mode($curchan, "-o", $curnick);
 	}
-      }
-
     }
 
     $last_mode_change_time = $current_mode_change_time;
@@ -65,7 +57,11 @@ sub modechange {
     $last_mode_changer{'chan'} = $curchan;
     $last_mode_changer{'mode'} = $curmode;
     $last_mode_changer{'target'} = $curtarget;
-
+    if($last_mode_changer{'nick'} eq $curnick) {
+	$last_mode_changer{'deops'} += $deops;
+    } else {
+	$last_mode_changer{'deops'} = 0;
+    }
 }
 
 1;
