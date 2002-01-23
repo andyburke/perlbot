14501d9be9eb9dc48bf4ada6e93dab19a7aa9dfd
Andy Burke <aburke@bitflood.org>
2002-01-23 18:01:18 +0000

    removed the unnecessary Perlbot::username function

--- perlbot/Perlbot.pm
+++ perlbot/Perlbot.pm
@@ -46,7 +46,7 @@ require Exporter;
 	     &parse_config &write_config
 	     &notify_users &to_channel &from_channel
 	     &validate_hostmask &host_to_user
-	     &username &update_user
+	     &update_user
 	     &validate_plugin &load_one_plugin &start_plugin
 	     &add_handler &unload_one_plugin
 	     &shutdown_bot
@@ -237,20 +237,6 @@ sub host_to_user {
     if($debug) { print "Multiple users matched $realmask !\n"; }
   }
   return undef;
-}
-
-sub username {
-  my $user = shift;
-  my $username;
-
-  foreach my $tempname (keys(%users)) {
-    if($user == $users{$tempname}) {
-      $username = $tempname;
-      last;
-    }
-  }
-
-  return $username;
 }
 
 sub update_user {
