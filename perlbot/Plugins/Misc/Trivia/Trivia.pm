dca3730901d5c81a814df0195da95008fdd8da4f
Andy Burke <aburke@bitflood.org>
2002-02-22 18:32:22 +0000

    checkin

---
+++ perlbot/Plugins/Misc/Trivia/Trivia.pm
@@ -0,0 +1,215 @@
+package Perlbot::Plugin::Trivia;
+
+use Perlbot::Plugin;
+@ISA = qw(Perlbot::Plugin);
+
+use strict;
+use Time::HiRes qw(time);
+
+sub init {
+  my $self = shift;
+
+  $self->want_fork(0);
+  $self->want_msg(0);
+  
+  $self->{state} = 'idle';
+  $self->{question} = -1;
+
+  $self->{questions} = {};
+
+  print "file: " . File::Spec->catfile($self->{directory}, 'trivia') . "\n";
+
+  open(TRIVIA, File::Spec->catfile($self->{directory}, 'trivia'));
+  my $i = 0;
+  while(my $q = <TRIVIA>) {
+    chomp $q;
+    $self->{questions}{$i} = $q;
+    $i++;
+  }
+  close(TRIVIA);
+
+  $self->{players} = {};
+
+  $self->{askedtime} = 0;
+  
+  $self->{fastest} = 9999;
+  $self->{usersfastest} = {};
+
+  $self->{curquestion} = 1;
+  $self->{numquestions} = 0;
+  $self->{answered} = 0;
+
+  $self->hook('trivia', \&starttrivia);
+  $self->hook('stoptrivia', \&stoptrivia);
+  $self->hook_regular_expression('.*', \&answer);
+
+}
+
+sub starttrivia {
+  my $self = shift;
+  my $user = shift;
+  my $text = shift;
+  
+  if($self->{state} ne 'idle') {
+    $self->reply_error('A trivia game is already in progress!');
+    return;
+  }
+
+  $self->{state} = 'playing';
+
+  my ($numquestions) = $text =~ /(\d+)/;
+  $numquestions ||= 15;
+
+  $self->{numquestions} = $numquestions;
+  $self->reply("Starting a new trivia game of $numquestions questions!");
+
+  $self->{perlbot}{ircconn}->schedule(10, sub { $self->askquestion() });
+}
+
+sub stoptrivia {
+  my $self = shift;
+
+  $self->{state} = 'idle';
+  $self->reply('Trivia stopped!');
+  $self->endofgame();
+
+}
+
+sub answer {
+  my $self = shift;
+  my $user = shift;
+  my $text = shift;
+
+  my $nick = $self->{lastnick};
+
+  if($self->{state} ne 'asked' || $self->{answered}) {
+    return;
+  }
+
+  my ($category, $question, $answer) = split(':::', $self->{questions}{$self->{question}});
+
+  if(lc($text) eq lc($answer)) {
+    $self->{answered} = 1;
+    $self->{players}{$nick}++;
+    my $timediff = sprintf("%0.2f", time() - $self->{askedtime});
+    if(!exists($self->{usersfastest}{$nick})) {
+      $self->{usersfastest}{$nick} = $timediff;
+    }
+    $self->reply("The answer was: $answer");
+    $self->reply("Winner: $nick  Time: $timediff (fastest: $self->{usersfastest}{$nick}) Wins: $self->{players}{$nick}");
+    if($timediff < $self->{fastest}) {
+      $self->{fastest} = $timediff;
+    }
+    if($timediff < $self->{usersfastest}{$nick}) {
+      $self->{usersfastest}{$nick} = $timediff;
+    }
+
+    $self->{curquestion}++;
+    $self->{perlbot}{ircconn}->schedule(10, sub { $self->askquestion() });
+
+  }
+}
+
+sub askquestion {
+  my $self = shift;
+
+  if($self->{state} eq 'idle') {
+    return;
+  }
+  
+  if($self->{curquestion} <= $self->{numquestions}) {
+
+    $self->{question} = rand(100000) % keys(%{$self->{questions}}) + 1;
+    $self->{state} = 'asked';
+    
+    my ($category, $question, $answer) = split(':::', $self->{questions}{$self->{question}});
+    my $curquestion = $self->{curquestion};
+
+    $self->{answered} = 0;
+    $self->reply("${curquestion}. [${category}] $question");
+    $self->{askedtime} = time();
+    $self->{perlbot}{ircconn}->schedule(10, sub { $self->hint("$curquestion") });
+    $self->{perlbot}{ircconn}->schedule(30, sub { $self->notanswered("$curquestion") });
+  } else {
+    $self->reply("Game over.");
+    $self->endofgame();
+    $self->{state} = 'idle';
+    $self->{curquestion} = 1;
+  }
+}
+
+sub hint {
+  my $self = shift;
+  my $question = shift;
+
+  if($self->{state} ne 'asked' || $self->{curquestion} != $question) {
+    return;
+  }
+
+  my ($category, $q, $a) = split(':::', $self->{questions}{$self->{question}});
+
+  my $blackout;
+  if(length($a) > 3) {
+    $blackout = (length($a) / 2) + (rand(1000) % (length($a)/4)) - 1;
+  } else {
+    $blackout = length($a);
+  }
+
+  my $hint;
+  if(int(rand(2)) == 1) {
+    substr($a, 0, (length($a) - $blackout)) =~ tr[ A-Za-z0-9][ _];
+  } else {
+    substr($a, (length($a) - $blackout)) =~ tr[ A-Za-z0-9][ _];
+  }
+
+  $self->reply("Hint: $a");
+}
+
+sub notanswered {
+  my $self = shift;
+  my $question = shift;
+
+  print "curquestion: $self->{curquestion} / passed: $question\n";
+
+  if($self->{state} ne 'asked' || $self->{curquestion} != $question) {
+    return;
+  }
+
+  my ($category, $questiontext, $answer) = split(':::', $self->{questions}{$self->{question}});
+
+  $self->reply("The answer was: $answer");
+  $self->{state} = 'playing';
+  $self->{perlbot}{ircconn}->schedule(10, sub { $self->askquestion() });
+  $self->{curquestion}++;
+
+}
+
+sub endofgame {
+  my $self = shift;
+  my $winner;
+  my $winnerscore = -1;
+
+  foreach my $nick (keys(%{$self->{players}})) {
+    if($self->{players}{$nick} > $winnerscore) {
+      $winner = $nick;
+      $winnerscore = $self->{players}{$nick};
+      $self->{players}{$nick} = 0; # reset wins
+    }
+  }
+
+  if($winnerscore == -1) {
+    return;
+  }
+
+  my $fastest = $self->{usersfastest}{$winner};
+
+  $self->reply("Trivia Winner is: $winner with $winnerscore wins and a fastest time of $fastest!");
+}
+
+1;
+
+
+
+
+
+
