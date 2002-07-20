8942554bbc0ef504749a158a390eb0a9858ccc8a
Jeremy Muhlich <jmuhlich@bitflood.org>
2002-07-20 09:36:23 +0000

    made StatsServer only fork to handle individual http connections

--- perlbot/Plugins/Misc/StatsServer/StatsServer.pm
+++ perlbot/Plugins/Misc/StatsServer/StatsServer.pm
@@ -5,7 +5,7 @@
 # TODO: make it use channel keys for password protection of the stats... ?
 #       make this thing die cleanly
 #       fork-at-birth not going to work here, bad idea anyway. set timeout?
-$       split up plugin forking logic so we can re-use parts here
+#       split up plugin forking logic so we can re-use parts here
 
 package Perlbot::Plugin::StatsServer;
 
@@ -17,38 +17,49 @@ use Perlbot::Utils;
 use HTTP::Daemon;
 use HTTP::Response;
 use HTTP::Headers;
+use HTTP::Status;
 use XML::Simple;
 
 our $VERSION = '1.0.0';
 
-sub new {
-  my $self = Perlbot::Plugin->new(@_);
-
-  my $server = HTTP::Daemon->new(LocalAddr => $self->config->value(server => 'hostname'),
-                                 LocalPort => $self->config->value(server => 'port'));
-
-  if (!$server) {
-    debug("Could not create StatsServer http server: $!");
-    return undef;
-  }
-
-  return $self;
-}
 
 sub init {
   my $self = shift;
 
+  my $server = HTTP::Daemon->new(LocalAddr => $self->config->value(server => 'hostname'),
+                                 LocalPort => $self->config->value(server => 'port'));
+  if (!$server) {
+    debug("Could not create StatsServer http server: $!");
+    $self->_shutdown;
+    return;
+  }
+  $self->{_server} = $server;
+
+  my $select = new IO::Select($server);
+  $self->{_select} = $select;
+
   $self->want_msg(0);
 
-  $self->hook_event('endofmotd', sub { $self->statsserver });
-  $self->hook_event('nomotd', sub { $self->statsserver });
   $self->hook_event('topic', sub { $self->set_topic });
   $self->hook(sub { $self->set_lastline });
 
+  $self->timer;
+}
+
+sub timer {
+  my $self = shift;
+
+  while ($self->{_select}->can_read(.0001)) {
+    my $connection = $self->{_server}->accept;
+    $self->handle_request($connection);
+  }
+
+  $self->perlbot->ircconn->schedule(1, sub { $self->timer });
 }
 
 sub handle_request {
   my $self = shift;
+  my ($connection) = @_;
 
   my $pid;
 
@@ -68,7 +79,6 @@ sub handle_request {
 
     while (my $request = $connection->get_request) {
       if ($request->method eq 'GET') {
-
         my $data = {};
 
         foreach my $channel (values %{$self->perlbot->channels}) {
@@ -76,19 +86,17 @@ sub handle_request {
           push @{$data->{channel}}, $chan_data;
         }
 
-        my $response_xml = XMLout($data, rootname => 'perlbot-statistics');
+        my $response_xml = qq{<?xml version="1.0"?>\n};
+        $response_xml .= XMLout($data, rootname => 'perlbot-statistics');
 
-        $connection->send_response(HTTP::Response->new(HTTP::Response::RC_OK,
-                                                       'yay!',
+        $connection->send_response(HTTP::Response->new(RC_OK, status_message(RC_OK),
                                                        HTTP::Headers->new(Content_Type => 'text/xml;'),
                                                        $response_xml));
       }
     }
     $connection->close;
 
-    $self->perlbot->empty_queue; # send all waiting events
-    $self->_shutdown();
-    $self->perlbot->shutdown();
+    exit;
   }
 }
 
