a31a5ab6fffb1f3f8edee39d619cca4c895b8ece
Andy Burke <aburke@bitflood.org>
2002-12-06 23:15:17 +0000

    Made to use Net::DNS

--- perlbot/Plugins/Misc/Host/Host.pm
+++ perlbot/Plugins/Misc/Host/Host.pm
@@ -8,6 +8,7 @@ use Perlbot::Plugin;
 use base qw(Perlbot::Plugin);
 
 use Perlbot::Utils;
+use Net::DNS;
 
 our $VERSION = '1.0.0';
 
@@ -27,11 +28,36 @@ sub host {
     $self->reply('You must specify a hostname to look up!');
   } else {
 
-    my @result = Perlbot::Utils::exec_command('host', $text);
+#    my @result = Perlbot::Utils::exec_command('host', $text);
 
-    foreach my $line (@result) {
-      $self->reply($line);
+#    foreach my $line (@result) {
+#      $self->reply($line);
+#    }
+
+    my $resolver = new Net::DNS::Resolver;
+    my $query = $resolver->search($text);
+
+    if($query) {
+      my @results = $query->answer();
+      my $ip;
+      my $realname;
+
+      foreach my $result (@results) {
+        if($result->type() eq 'CNAME') {
+          $realname = $result->cname();
+        }
+        if($result->type() eq 'A') {
+          $ip = $result->address();
+        }
+      }
+
+      $self->reply($text . " has address $ip ($realname)");
+    } else {
+      $self->reply_error("$text not found!");
     }
+
   }
 }
+
+
 
