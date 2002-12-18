260d5fd3f91d0070258ed3187119f8e1e7461e71
Jeremy Muhlich <jmuhlich@bitflood.org>
2002-12-18 06:34:47 +0000

    added comments

--- perlbot/Perlbot/PluginConfig.pm
+++ perlbot/Perlbot/PluginConfig.pm
@@ -1,9 +1,14 @@
 package Perlbot::PluginConfig;
 
 
+# This package subclasses Perlbot::Config, and simply overrides _value
+# to provide a Plugin object "rooted" at a given plugin's tree in the
+# config.
+
 use strict;
 use base qw(Perlbot::Config);
 use fields qw(plugin_name);
+
 
 sub new {
   my $class = shift;
@@ -16,6 +21,9 @@ sub new {
   return $self;
 }
 
+
+# Here we insert 'plugin',<plugin_name> at the front of the key list.
+# This affects all of get, set, array_*, hash_*, etc. on this object!
 sub _value : lvalue {
   my ($self, @keys) = @_;
   $self->SUPER::_value(plugin => $self->{plugin_name} => @keys);
