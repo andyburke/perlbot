ed5b32fa20e13ed319721cab3961c5540be6aa16
Andy Burke <aburke@bitflood.org>
2003-02-07 03:33:06 +0000

    little cleaning

--- perlbot/Plugins/Misc/LogServer/LogServer.pm
+++ perlbot/Plugins/Misc/LogServer/LogServer.pm
@@ -120,7 +120,10 @@ sub logserver {
           if($channel->logs->search({ initialdate => Date::Manip::UnixDate("$year/$month/$day-00:00:00",'%s'),
                                       finaldate   => Date::Manip::UnixDate("$year/$month/$day-23:59:59",'%s'),
                                       boolean     => 1 })) {
-            $cal->item($day)->wrap_content(HTML::Element->new('a', href => "/logserver/display?channel=" . $options->{channel} . "&year=${year}&month=${month}&day=${day}"));
+            $cal->item($day)->wrap_content(HTML::Element->new('a',
+                                                              href => "/logserver/display?channel="
+                                                              . $options->{channel}
+                                                              . "&year=${year}&month=${month}&day=${day}"));
           }
         }
         $response .= $cal->as_HTML();
@@ -133,8 +136,18 @@ sub logserver {
     }
 
     if(defined($options->{day}) && defined($options->{month}) && defined($options->{year})) {
-      my @events = $channel->logs->search({ initialdate => Date::Manip::UnixDate($options->{year} . "/" . $options->{month} . "/" . $options->{day} . "-00:00:00",'%s'),
-                                              finaldate   => Date::Manip::UnixDate($options->{year} . "/" . $options->{month} . "/" . $options->{day} . "23:59:59",'%s')});
+      my @events = $channel->logs->search({ initialdate => Date::Manip::UnixDate($options->{year}
+                                                                                 . "/"
+                                                                                 . $options->{month}
+                                                                                 . "/"
+                                                                                 . $options->{day}
+                                                                                 . "-00:00:00",'%s'),
+                                            finaldate   => Date::Manip::UnixDate($options->{year}
+                                                                                 . "/"
+                                                                                 . $options->{month}
+                                                                                 . "/"
+                                                                                 . $options->{day}
+                                                                                 . "-23:59:59",'%s')});
 
       my @loglines;
       while(my $event = shift @events) {
@@ -285,6 +298,8 @@ sub logserver {
                                       $initialmin,
                                       $initialsec);
 
+      print $initialdatestring . "\n";
+
       my $finalyear = $options->{finalyear};
       my $finalmonth = $options->{finalmonth};
       my $finalday = $options->{finalday};
@@ -299,8 +314,14 @@ sub logserver {
                                       $finalmin,
                                       $finalsec);
 
+      print $finaldatestring . "\n";
+
       my $initialdate = Date::Manip::UnixDate($initialdatestring,'%s');
       my $finaldate = Date::Manip::UnixDate($finaldatestring,'%s');
+
+      print localtime($initialdate) . "\n";
+      print localtime($finaldate) . "\n";
+
 
       my $nick = $options->{nick};
       my $type = $options->{type}; if($type eq 'all') { $type = undef; };
@@ -333,190 +354,6 @@ sub logserver {
       return $self->std_response($response);
     }                       
   }          
-
-=pod
-  if($chan && !$year && !$search) {
-    $response .= "<a href=\"/logs/${chan}/search\">[search]</a> <a href=\"/logs/${chan}\">[${chan}]</a> <a href=\"/logs\">[top]</a><p>";
-    if(!opendir(LOGLIST, File::Spec->catfile($self->logdir, $chan))) {
-      $response .= "No logs for channel: $chan";
-    } else {
-      my @tmpfiles =  readdir(LOGLIST);
-      my @logfiles = sort(@tmpfiles);
-      close LOGLIST;
-      
-      my @years;
-      
-      foreach my $logfile (@logfiles) {
-        if (!($logfile =~ /^\.\.?$/)) {
-          my ($tmpyear) = $logfile =~ /^(\d+)\./;
-          
-          if(!grep { /$tmpyear/ } @years) { push(@years, $tmpyear); }
-        }
-      }
-      
-      $response .= "<p><h2>Logs for $chan in:</h2>";
-      $response .= '<p><hr><p><ul>';
-      foreach my $year (@years) {
-        $response .= "<li><b><a href=\"/logs/${chan}/${year}\">$year</a></b>";
-      }
-      $response .= '</ul>';
-    }
-  }
-  
-  if($chan && $year && !$month && !$search) {
-    $response .= "<a href=\"/logs/${chan}/search\">[search]</a> <a href=\"/logs/${chan}\">[${chan}]</a> <a href=\"/logs\">[top]</a><p>";
-    
-    use HTML::CalendarMonth;
-    
-    if(!opendir(LOGLIST, File::Spec->catfile($self->logdir, $chan))) {
-      $response .= "No logs for channel: $chan";
-    } else {
-      my @tmpfiles =  readdir(LOGLIST);
-      my @logfiles = sort(@tmpfiles);
-      close LOGLIST;
-      
-      $response .= "<center><h1>Logs for channel: $chan</h1></center>";
-      $response .= '<p><hr>';
-      
-      $response .= "<p><center><h1>$year</h1></center>\n";
-      $response .= "<table width=\"100%\" border=\"0\">\n  <tr valign=\"center\" align=\"center\">\n";
-      foreach my $month ((1..12)) {
-        $response .= "    <td width=\"25%\" valign=\"center\" align=\"center\">\n";
-        $month = sprintf("%02d", $month);
-        my $cal = HTML::CalendarMonth->new(year => $year, month => $month);
-        foreach my $day ((1..31)) {
-          my $paddedday = sprintf("%02d", $day);
-          my $filename = "$year.$month.$paddedday"; 
-          if(grep { /\Q$filename\E/ } @logfiles) {
-            $cal->item($day)->wrap_content(HTML::Element->new('a', href => "/logs/${chan}/${year}/${month}/${paddedday}"));
-          }
-        }
-        $response .= $cal->as_HTML();
-        $response .= "</td>\n";
-        if($month % 4 == 0) { $response .="  </tr>\n  <tr>\n"; }
-      }
-      $response .= "</table>\n";
-    }
-  }
-  
-  if($chan && $year && $month && !$day && !$search) {
-    $response .= "<a href=\"/logs/${chan}/search\">[search]</a> <a href=\"/logs/${chan}\">[${chan}]</a> <a href=\"/logs\">[top]</a><p>";
-    use HTML::CalendarMonth;
-    
-    if(!opendir(LOGLIST, File::Spec->catfile($self->logdir, $chan))) {
-      $response .= "No logs for channel: $chan";
-    } else {
-      my @tmpfiles =  readdir(LOGLIST);
-      my @logfiles = sort(@tmpfiles);
-      close LOGLIST;
-      
-      $response .= "<center><h1>Logs for channel: $chan</h1></center>";
-      $response .= '<p><hr>';
-      
-      $response .= "<p><center><h1>$year</h1></center>\n";
-      $month = sprintf("%02d", $month);
-      my $cal = HTML::CalendarMonth->new(year => $year, month => $month);
-      foreach my $day ((1..31)) {
-        my $paddedday = sprintf("%02d", $day);
-        my $filename = "$year.$month.$paddedday";
-        if(grep { /\Q$filename\E/ } @logfiles) {
-          $cal->item($day)->wrap_content(HTML::Element->new('a', href => "/logs/${chan}/${year}/${month}/${paddedday}"));
-        }
-      }
-      $response .= $cal->as_HTML();
-    }
-  }
-  
-  if($chan && $year && $month && $day && !$search) {
-    $response .= "<a href=\"/logs/${chan}/search\">[search]</a> <a href=\"/logs/${chan}\">[${chan}]</a> <a href=\"/logs\">[top]</a><p>";
-    my $filename = File::Spec->catfile($self->logdir, $chan, "$year.$month.$day");
-    
-    if(open(FILE, $filename)) {
-      my @lines = <FILE>;
-      close(FILE);
-      
-      foreach (@lines) {
-        s/\&/\&amp\;/g;
-        s/</\&lt\;/g;
-        s/>/\&gt\;/g;
-        s/(\&lt\;.*?\&gt\;)/<b>$1<\/b>/;
-        s|^(\d+:\d+:\d+) \* (\w+) (.*)|$1 * <b>$2</b> $3|;
-        s|^(\d+:\d+:\d+\s)(.*? joined \#.*?$)|$1<font color=\"blue\">$2</font>|;
-        s|^(\d+:\d+:\d+\s)(.*? left \#.*?$)|$1<font color=\"blue\">$2</font>|;
-        s|^(\d+:\d+:\d+\s)(\[.*?\])|$1<font color=\"red\">$2</font>|;
-        s|(\d+\:\d+:\d+)|<a name=\"$1\">$1</a>|;
-        s#(\w+://.*?|www\d*\..*?|ftp\d*\..*?|web\d*\..*?)(\s+|'|,|$)#<a href="$1">$1</a>$2#;
-        $response .= "<tt>$_</tt><br>";
-      }
-    }
-  }                               
-  
-  if($search) {
-    if(!scalar @searchwords) {
-      $response .= "<a href=\"/logs/${chan}/search\">[search]</a> <a href=\"/logs/${chan}\">[${chan}]</a> <a href=\"/logs\">[top]</a><p>";
-      $response .= "<h2>Search logs for channel: $chan</h2><p>";
-      $response .= "<form method=\"get\" action=\"/logs/${chan}/search\">";
-      $response .= "Enter words to search for: <input type=\"text\" name=\"words\"  />";
-      $response .= "<input type=\"submit\" name=\".submit\" />";
-      $response .= "</form>";
-    } else {
-      $response .= "<a href=\"/logs/${chan}/search\">[search]</a> <a href=\"/logs/${chan}\">[${chan}]</a> <a href=\"/logs\">[top]</a><p>";
-      
-      if(opendir(DIR, File::Spec->catfile($self->logdir, $chan))) {
-        my @tmp = readdir(DIR);
-        my @files = sort(@tmp);
-        
-        my $results_found = 0;
-        
-        foreach my $file (@files) {
-          open(FILE, File::Spec->catfile($self->logdir, $chan, $file));
-          my @lines = <FILE>;
-          close FILE;
-          
-          foreach my $word (@searchwords) {
-            @lines = grep(/\Q$word\E/i, @lines);  
-          }
-          
-          foreach (@lines) {
-            s/</&lt;/g;
-            s/>/&gt;/g;
-          }
-          
-          if (@lines) {
-            $results_found = 1;
-            ($year, $month, $day) = split(/\./, $file);
-            $response .= "<p><b><a href=\"/logs/${chan}/${year}/${month}/${day}\">$file</a></b>";
-            $response .= "<p>";
-            foreach (@lines) {
-              s/</\&lt\;/g;
-              s/>/\&gt\;/g;
-              s/(\&lt\;.*?\&gt\;)/<b>$1<\/b>/;
-              s|^(\d+:\d+:\d+) \* (\w+) (.*)|$1 * <b>$2</b> $3|;
-              s|^(\d+:\d+:\d+\s)(.*? joined \#.*?$)|$1<font color=\"blue\">$2</font>|;
-              s|^(\d+:\d+:\d+\s)(.*? left \#.*?$)|$1<font color=\"blue\">$2</font>|;
-              s|^(\d+:\d+:\d+\s)(\[.*?\])|$1<font color="red">$2</font>|;
-              s|(\d+:\d+:\d+)|<a href="/logs/${chan}/${year}/${month}/${day}#$1">$1</a>|;
-              s#(\w+://.*?|www\d*\..*?|ftp\d*\..*?|web\d*\..*?)(\s+|'|,|$)#<a href="$1">$1</a>$2#;
-              $response .= "<tt>$_</tt><br>";
-              
-            }
-          }
-        }
-        
-        if(!$results_found) {
-          $response .= '<center><h2>No results found for: ' . join(' ', @searchwords) . '</h2></center>';
-        }
-        
-        closedir(DIR);
-      }
-    }
-  }
-
-=cut
-
-  $response .= '</body></html>';
-
-  return $self->std_response($response);
 }
 
 sub std_response {
