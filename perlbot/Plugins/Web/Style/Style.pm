5d911bb578fff3fa38494563cb9cd3ef03c2b528
Andy Burke <aburke@bitflood.org>
2003-04-10 07:50:09 +0000

    Allow you to spec a file in the config

--- perlbot/Plugins/Web/Style/Style.pm
+++ perlbot/Plugins/Web/Style/Style.pm
@@ -19,17 +19,24 @@ our $VERSION = '1.0.0';
 sub init {
   my $self = shift;
 
-  $self->{stylefile} = File::Spec->catfile($self->{directory}, 'perlbot.css');
-
   $self->hook_web('perlbot.css', \&servecss, 'Site-Wide Style Sheet');
 }
+
+sub set_initial_config_values {
+  my $self = shift;
+
+  $self->config->set('css-file', File::Spec->catfile($self->{directory}, 'perlbot.css'));
+
+  return 1;
+}
+
 
 sub servecss {
   my $self = shift;
   my @args = @_;
 
   my $css;
-  open(CSS, $self->{stylefile});
+  open(CSS, $self->config->get('css-file'));
   while(my $line = <CSS>) {
     chomp $line;
     $css .= $line;
