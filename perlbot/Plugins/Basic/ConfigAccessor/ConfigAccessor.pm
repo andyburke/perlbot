9a4de7247ee0e7b8181dac084c55f4c65fd6d596
Andy Burke <aburke@bitflood.org>
2003-04-11 20:55:09 +0000

    get/set config values on the fly

---
+++ perlbot/Plugins/Basic/ConfigAccessor/ConfigAccessor.pm
@@ -0,0 +1,39 @@
+package Perlbot::Plugin::ConfigAccessor;
+
+use strict;
+use base qw(Perlbot::Plugin);
+use Perlbot;
+
+our $VERSION = '0.0.1';
+
+sub init {
+  my $self = shift;
+
+  $self->want_public(0);
+  $self->want_fork(0);
+
+  $self->hook_admin('config', \&configaccessor);
+}
+
+sub configaccessor {
+  my $self = shift;
+  my $user = shift;
+  my $text = shift;
+
+  my ($action, $args) = split(/ /, $text, 2);
+  
+  debug("action: $action / args: $args", 5);
+
+  if(lc($action) eq 'get') {
+    debug("Getting config value: $args", 4);
+    $self->reply($self->perlbot->config->get(eval "$args"));
+  } elsif(lc($action) eq 'set') {
+    my ($key, $value) = split(/,/, $args, 2);
+    debug("Setting config value: $key to value: $value", 4);
+    $self->reply($self->perlbot->config->set(eval "$key", $value));
+  } else {
+    $self->reply_error("Unknown config command: $action!");
+  }
+}
+
+1;
